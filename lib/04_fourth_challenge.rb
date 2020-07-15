4a4472f74b1bfae26dd6c08f5593fe40ff906ff8
Anson Nickel <anson.nickel@gmail.com>
2020-07-15 19:49:27 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -4,7 +4,6 @@ def fourth_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
-      binding.pry
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
